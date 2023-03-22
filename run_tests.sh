0d601e6ef41f4d87c734ce96d1d6e9c12ab4907c
Ronald Krist <ronald.krist@cesnet.cz>
2023-03-22 15:22:22 +0100

    added actions and test

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+MODEL="thesis"
+VENV=".model_venv"
+export OPENSEARCH_PORT=9400
+#cd $(dirname $0)/..
+if test -d ./tests/$MODEL; then
+	rm -rf ./tests/$MODEL
+fi
+oarepo-compile-model ./tests/$MODEL.yaml --output-directory ./tests/$MODEL --profile model,drafts -vvv
+python3 -m venv $VENV
+. $VENV/bin/activate
+pip install -U setuptools pip wheel
+pip install "./tests/$MODEL[tests]"
+pytest tests/$MODEL/tests
