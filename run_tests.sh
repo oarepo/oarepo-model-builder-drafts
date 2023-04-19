90733617f1fc1631be9817da24e5609788fecf9c
Ronald Krist <ronald.krist@cesnet.cz>
2023-04-19 10:45:16 +0200

    tests directory moved

--- run_tests.sh
+++ run_tests.sh
@@ -5,12 +5,12 @@ MODEL="thesis"
 VENV=".model_venv"
 export OPENSEARCH_PORT=9400
 #cd $(dirname $0)/..
-if test -d ./tests/$MODEL; then
-	rm -rf ./tests/$MODEL
+if test -d ./build-tests/$MODEL; then
+	rm -rf ./build-tests/$MODEL
 fi
-oarepo-compile-model ./tests/$MODEL.yaml --output-directory ./tests/$MODEL --profile model,drafts -vvv
+oarepo-compile-model ./build-tests/$MODEL.yaml --output-directory ./build-tests/$MODEL --profile model,drafts -vvv
 python3 -m venv $VENV
 . $VENV/bin/activate
 pip install -U setuptools pip wheel
-pip install "./tests/$MODEL[tests]"
-pytest tests/$MODEL/tests
+pip install "./build-tests/$MODEL[tests]"
+pytest build-tests/$MODEL/tests
