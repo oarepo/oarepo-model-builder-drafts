e1e093bc8211709915032c057321b9bf5896905a
Ronald Krist <ronald.krist@cesnet.cz>
2023-04-25 10:52:46 +0200

    sample record fixture and test dependencies fix

--- oarepo_model_builder_drafts/tests/__init__.py
+++ oarepo_model_builder_drafts/tests/__init__.py
@@ -1,3 +1,4 @@
 TEMPLATES = {
     "drafts-test-resources": "templates/invenio_test_resources.py.jinja2",
+    "drafts-sample-record-fixtures": "templates/drafts_sample_record_fixture.py.jinja2"
 }
--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ set -e
 
 MODEL="thesis"
 VENV=".model_venv"
-
+#export OPENSEARCH_PORT=9400
 if test -d ./build-tests/$MODEL; then
 	rm -rf ./build-tests/$MODEL
 fi
@@ -11,5 +11,7 @@ oarepo-compile-model ./build-tests/$MODEL.yaml --output-directory ./build-tests/
 python3 -m venv $VENV
 . $VENV/bin/activate
 pip install -U setuptools pip wheel
+pip install 'oarepo>=11.0.26,<12.0.0'
+pip uninstall -y invenio_oauth2server
 pip install "./build-tests/$MODEL[tests]"
 pytest build-tests/$MODEL/tests
