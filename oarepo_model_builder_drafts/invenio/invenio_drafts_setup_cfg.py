4077ad0cffbc5e096c7159bc87ed8214b8010e24
Mirek Simek <miroslav.simek@gmail.com>
2023-09-24 20:35:27 +0200

    Integrated published service to draft builder

--- oarepo_model_builder_drafts/invenio/invenio_drafts_setup_cfg.py
+++ oarepo_model_builder_drafts/invenio/invenio_drafts_setup_cfg.py
@@ -11,3 +11,6 @@ class InvenioDraftsSetupCfgBuilder(OutputBuilder):
         output: CFGOutput = self.builder.get_output("cfg", "setup.cfg")
 
         output.add_dependency("invenio-drafts-resources", ">=1.0.4")
+
+        if not self.current_model.definition["published-service"].get("skip"):
+            output.add_dependency("oarepo-published-service", ">=1.0.0")
