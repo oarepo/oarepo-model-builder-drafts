3be63a8321cd967f2351f6f2dde68f46c6ff1e0e
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2025-06-19 12:10:27 +0200

    special facets moved to component builder

--- oarepo_model_builder_drafts/datatypes/components/draft_model/record.py
+++ oarepo_model_builder_drafts/datatypes/components/draft_model/record.py
@@ -2,10 +2,78 @@ from oarepo_model_builder.datatypes import DataType, ModelDataType
 from oarepo_model_builder.datatypes.components import RecordModelComponent
 from oarepo_model_builder.datatypes.components.model.utils import set_default
 
+from oarepo_model_builder.datatypes.components.facets import FacetDefinition
+from oarepo_model_builder.utils.python_name import Import
 
 class DraftRecordModelComponent(RecordModelComponent):
     eligible_datatypes = [ModelDataType]
     dependency_remap = RecordModelComponent
+    def process_facets(self, datatype, section, **__kwargs):
+
+        if datatype.root.profile != "record":
+            return section
+        facets =[
+        FacetDefinition(
+            path="record_status",
+            dot_path="record_status",
+            searchable=True,
+            imports=[
+                Import(
+                    import_path="invenio_records_resources.services.records.facets.TermsFacet",
+                    alias=None,
+                )
+            ],
+            facet_groups={"_default": 100000},
+            facet=None,
+            field="TermsFacet(field='record_status', label =_('record_status'))",
+        ),
+
+        FacetDefinition(
+            path="has_draft",
+            dot_path="has_draft",
+            searchable=True,
+            imports=[
+                Import(
+                    import_path="invenio_records_resources.services.records.facets.TermsFacet",
+                    alias=None,
+                )
+            ],
+            facet_groups={"_default": 100000},
+            facet=None,
+            field="TermsFacet(field='has_draft', label =_('has_draft'))",
+        ),
+
+        FacetDefinition(
+            path="expires_at",
+            dot_path="expires_at",
+            searchable=True,
+            imports=[
+                Import(
+                    import_path="oarepo_runtime.services.facets.date.DateTimeFacet",
+                    alias=None,
+                )
+            ],
+            facet_groups={"_default": 100000},
+            facet=None,
+            field="DateTimeFacet(field='expires_at', label=_('expires_at.label'))",
+        ),
+        FacetDefinition(
+            path="fork_version_id",
+            dot_path="fork_version_id",
+            searchable=True,
+            imports=[
+                Import(
+                    import_path="invenio_records_resources.services.records.facets.TermsFacet",
+                    alias=None,
+                )
+            ],
+            facet_groups={"_default": 100000},
+            facet=None,
+            field="TermsFacet(field='fork_version_id', label=_('fork_version_id.label'))",
+        ),
+    ]
+        section.config["facets"] = facets
+        return section
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
         if datatype.root.profile not in {"record", "draft"}:
